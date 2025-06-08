6d49b4a9a294751d335683a7d4494de6754bdf25
eustachekamala <eustachejared25@gmail.com>
2025-06-08 14:49:06 +0300

    stack that is a LIFO data structure, stores objects into a stort of vertical lower

---
+++ Stack/src/main/java/com/webapp/servlet/Main.java
@@ -0,0 +1,34 @@
+package com.webapp.servlet;
+
+import java.util.Stack;
+
+
+public class Main {
+    public static void main(String[] args) {
+        /*
+        * Stack: LIFO data structure: Last-In First-Out
+        * Stores objects into a sort of 'vertical lower'
+        * push() to add to the top
+        * pop() to remove from the top
+        */
+
+        Stack<String> stack = new Stack<>();
+
+        stack.push("Minecraft");
+        stack.push("Java");
+        stack.push("Python");
+        stack.push("PHP");
+
+        for (int i = 0; i < 10; i++) {
+            stack.push("Java");
+        }
+        System.out.println(stack);
+
+        /* Uses of stacks ?
+         *  1. undo/redo features in text editors
+         *  2. moving back/forward through browser history
+         *  3. backtracking algorithms (maze, file directories)
+         *  4. calling functions (call stack)
+         */
+    }
+}
