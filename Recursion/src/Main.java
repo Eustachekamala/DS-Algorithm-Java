39fc950f7dee3627194f3e5ba5f5aa7bbd610673
eustachekamala <eustachejared25@gmail.com>
2025-06-17 17:00:54 +0300

    added a function of factorial num

--- Recursion/src/Main.java
+++ Recursion/src/Main.java
@@ -9,6 +9,12 @@ public class Main {
                         * Recursive case – where the function calls itself with smaller input
          */
         walk(5);
+        System.out.println(factorial(7));
+    }
+
+    private static int factorial(int num) {
+        if (num == 1) return 1;
+        return num * factorial(num - 1);
     }
 
     private static void walk(int steps) {
