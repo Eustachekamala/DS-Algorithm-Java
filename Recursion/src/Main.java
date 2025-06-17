b87631fc7b497939c14e3a41e20e3833cedc30a9
eustachekamala <eustachejared25@gmail.com>
2025-06-17 16:58:42 +0300

    recursion is when a method calls itself to solve a smaller part of a problem

---
+++ Recursion/src/Main.java
@@ -0,0 +1,19 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        /*
+         * Recursion =  Recursion is when a method calls itself to solve a smaller part of a problem.
+                        It typically includes:
+                        * Base case – the condition to stop recursion.
+                        * Recursive case – where the function calls itself with smaller input
+         */
+        walk(5);
+    }
+
+    private static void walk(int steps) {
+        if (steps < 1) return ; // base case
+        System.out.println("You take a step!");
+        walk(steps - 1); // recursion
+    }
+}
