c47d26d791e50090aad9f146f71e32d039adf22d
eustachekamala <eustachejared25@gmail.com>
2025-10-13 14:27:40 +0300

    updated the method bubbleSort to return the value of sorting after each iteration

--- BubbleSort/src/Main.java
+++ BubbleSort/src/Main.java
@@ -11,24 +11,34 @@ public class Main {
          *              Small data set = okay-ish
          *              large data set = bad
          */
+        int[] nums = {9, 6,8,2,3,5,1,4,7};
+        System.out.print("Before Bubble Sort: ");
+        for(int num : nums){
+            System.out.print(num + " ");
+        }
 
-        int[] arr = {9,1,4,6,8,7,5,3,2,0};
-        bubbleSort(arr);
-        for (int i : arr){
-            System.out.print(i);
+        bubbleSort(nums);
+        System.out.println();
+        System.out.print("After Bubble Sort: ");
+        for (int num : nums){
+            System.out.print(num + " ");
         }
     }
 
-    private static void bubbleSort(int[] arr) {
-        for (int i = 0; i < arr.length - 1; i++){
-            for (int j = 0; j < arr.length - i - 1; j++){
-                if (arr[j] > arr[j+1]){
-                    int temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;
+    private static void bubbleSort(int[] nums) {
+        int size = nums.length;
+        for(int i = 0; i < size - 1; i++){
+            for(int j = 0; j < size - i - 1; j++){
+                if (nums[j] > nums[j+1]){
+                    int temp = nums[j];
+                    nums[j] = nums[j+1];
+                    nums[j+1] = temp;
+                }
+                System.out.println();
+                for (int num : nums){
+                    System.out.print(num + " ");
                 }
             }
         }
     }
-
 }
